34fe4b50f88fb703eea300413943f1c3ee616ddd
Quenos <coen.kuijpers@asild.eu>
2024-07-19 16:54:23 +0200

    Added blog

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 // Assuming you have a way to securely store and retrieve the token
 // For example, you might have it in a separate config file or get it from the server
-const STRAPI_TOKEN = '5548d7aa99974cc013b6c34b718c14028b2f22b962471776513b9e4eaea96ff1307fd0fe14326a7be2960ac26641fda98913ca5dd5b83ed8c0c66c11becf033a01712b707c4f5520d70e9e8c2814ce98f118d08bb67dd3ff316197b09a66545c732056b8e66ce7d27a1af8e3da4abb60f190b49df0f2c2eea17c49b50aa20793';
+const STRAPI_TOKEN = '';
 
 let currentPage = 1;
 const postsPerPage = 10;
